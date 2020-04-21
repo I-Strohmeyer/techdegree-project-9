95f079e9325bd3d30b8083b1e964bdb499705ab7
I-Strohmeyer <50380835+I-Strohmeyer@users.noreply.github.com>
2020-04-21 22:08:15 +0200

    nav buttons

---
+++ js/app.js
@@ -0,0 +1,29 @@
+// Variables
+
+const aboutbtn = document.getElementById('aboutbtn');
+const projectbtn = document.getElementById('projectbtn');
+const contactbtn = document.getElementById('contactbtn');
+
+const about = document.getElementById('about');
+const project = document.getElementById('projects');
+const contact = document.getElementById('contact');
+
+// Events
+
+aboutbtn.addEventListener( 'click', event => {
+    about.style.display = "block";
+    projects.style.display = "none";
+    contact.style.display = "none";
+});
+
+projectbtn.addEventListener( 'click', event => {
+    projects.style.display = "block";
+    about.style.display = "none";
+    contact.style.display = "none";
+});
+
+contactbtn.addEventListener( 'click', event => {
+    contact.style.display = "block";
+    projects.style.display = "none";
+    about.style.display = "none";
+});
