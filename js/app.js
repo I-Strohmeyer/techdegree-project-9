d8e4b727ce24a9e81a342892a142b5dcfe19b9d6
IAS <is@daypaio.com>
2020-04-26 00:58:28 +0200

    mobile modal + refine

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 // Variables
+//____________________
+
+const aboutm = document.getElementById('aboutm');
+const projectm = document.getElementById('projectm');
+const contactm = document.getElementById('contactm');
 
 const aboutbtn = document.getElementById('aboutbtn');
 const projectbtn = document.getElementById('projectbtn');
@@ -8,22 +13,82 @@ const about = document.getElementById('about');
 const project = document.getElementById('projects');
 const contact = document.getElementById('contact');
 
-// Events
+// Get the modal
+const modal = document.getElementById("myModal");
 
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// burger svg that will get clicked to pull up menu on mobile
+const menubutton = document.getElementById('mmenu');
+
+
+// Events
+//_______________________
+
+
+//mobile menu modal
+menubutton.addEventListener('click', event => {
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// closes modal when you click outside
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}
+
+// mobile menu selection when modal is open
+aboutm.addEventListener('click', event => {
+    modal.style.display = "none";
+    about.style.display = "block";
+    projects.style.display = "none";
+    contact.style.display = "none";
+})
+
+// mobile menu selection when modal is open
+projectm.addEventListener('click', event => {
+    modal.style.display = "none";
+    about.style.display = "none";
+    projects.style.display = "block";
+    contact.style.display = "none";
+})
+
+// mobile menu selection when modal is open
+contactm.addEventListener('click', event => {
+    modal.style.display = "none";
+    about.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "block";
+})
+
+
+// switch content for menu entries (about)
 aboutbtn.addEventListener( 'click', event => {
     about.style.display = "block";
     projects.style.display = "none";
     contact.style.display = "none";
 });
 
+// switch content for menu entries (project)
 projectbtn.addEventListener( 'click', event => {
     projects.style.display = "block";
     about.style.display = "none";
     contact.style.display = "none";
 });
 
+// switch content for menu entries (contact)
 contactbtn.addEventListener( 'click', event => {
     contact.style.display = "flex";
     projects.style.display = "none";
     about.style.display = "none";
 });
+
+
+
