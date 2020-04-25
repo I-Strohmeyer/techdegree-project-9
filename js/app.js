4482b17512a167e4129fa74afed83c9393de88e9
IAS <is@daypaio.com>
2020-04-26 01:46:58 +0200

    new tabs for projects

--- js/app.js
+++ js/app.js
@@ -22,9 +22,34 @@ const span = document.getElementsByClassName("close")[0];
 // burger svg that will get clicked to pull up menu on mobile
 const menubutton = document.getElementById('mmenu');
 
+const project5 = document.getElementById('project5');
+const project6 = document.getElementById('project6');
+const project7 = document.getElementById('project7');
+const project8 = document.getElementById('project8');
+
 
 // Events
 //_______________________
+
+// opens new tab for project 5
+project5.addEventListener('click', event => {
+    window.open('https://i-strohmeyer.github.io/techdegree-project-5/', '_blank');
+});
+
+// opens new tab for project 6
+project6.addEventListener('click', event => {
+    window.open('https://i-strohmeyer.github.io/techdegree-project-6/', '_blank');
+});
+
+// opens new tab for project 7
+project7.addEventListener('click', event => {
+    window.open('https://i-strohmeyer.github.io/techdegree-project-7/', '_blank');
+});
+
+// opens new tab for project 8
+project8.addEventListener('click', event => {
+    window.open('https://i-strohmeyer.github.io/techdegree-project-8/', '_blank');
+});
 
 
 //mobile menu modal
